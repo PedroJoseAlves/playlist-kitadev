e16f9b8b61c861a51e38ec75c6dc02599257e018
PedrojoseAlves <pedrojosealves3@gmail.com>
2024-07-08 22:23:53 +0100

    connect db

---
+++ index.js
@@ -0,0 +1,16 @@
+require("dotenv").config()
+
+const express = require('express')
+const connectToDb= require("./database/db")
+
+const app = express()
+
+const port = process.env.PORT || 5000;
+
+connectToDb();
+
+app.get('/hello', (req, res) => {
+    res.send("Pedro Alves")
+})
+
+app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`))
